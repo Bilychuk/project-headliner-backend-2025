d457d871e1a8d129489955f1f0977284067416c1
NataliiaHolovash <n.golovash@ukr.net>
2025-06-27 13:44:56 +0200

    change bcrypt

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,12 +1,7 @@
-import bcrypt from 'bcryptjs';
 import { registerUser } from '../services/auth.js';
 
 export const registerUserController = async (req, res) => {
-  const { name, email, password } = req.body;
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  const user = await registerUser({ name, email, password: hashedPassword });
+  const user = await registerUser(req.body);
 
   res.status(201).json({
     status: 201,
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,5 +1,13 @@
+import bcrypt from 'bcrypt';
 import  UsersCollection  from '../db/models/user.js';
 
 export const registerUser = async (payload) => {
-  return await UsersCollection.create(payload);
+  const hashedPassword = await bcrypt.hash(payload.password, 10);
+
+  const user = await UsersCollection.create({
+    ...payload,
+    password: hashedPassword,
+  });
+
+  return user;
 };
