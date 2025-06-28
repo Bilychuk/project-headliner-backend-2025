be198fda6ea62df947907a758c9f1c6a48488f88
Bilychuk Kateryna <bilychukkateryna@ukr.net>
2025-06-28 08:14:06 +0300

    correct-validation

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 
 export const registerUserSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
+  name: Joi.string().min(3).max(16).required(),
+  email: Joi.string().email().max(128).required(),
+  password: Joi.string().min(8).max(128).required(),
 });
