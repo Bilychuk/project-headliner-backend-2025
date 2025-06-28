05fc36c56f921c1b5923d1f32f55f25ab9c4400a
Bilychuk Kateryna <bilychukkateryna@ukr.net>
2025-06-28 19:52:30 +0300

    correct users response

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -2,10 +2,8 @@ export const getCurrentUserController = async (req, res) => {
   const user = req.user;
 
   res.status(200).json({
-    status: 'success',
-    code: 200,
-    data: {
-      user: user,
-    },
+    status: 200,
+    message: 'Successfully found user',
+    data: user,
   });
 };
