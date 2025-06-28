df8b24a6da017cd80c0f9b71a765e73edbbc717c
Yavor <pojardim@gmail.com>
2025-06-28 10:24:04 +0300

    fix login

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,9 +2,11 @@ import createHttpError from 'http-errors';
 import bcrypt from 'bcrypt';
 import { randomBytes } from 'crypto';
 import { FIFTEEN_MINUTES, THIRTY_DAYS } from '../constants/index.js';
+import UsersCollection from '../db/models/user.js';
+import Session from '../db/models/session.js';
 
 export const loginUser = async (payload) => {
-  const user = await Model.findOne({ email: payload.email });
+  const user = await UsersCollection.findOne({ email: payload.email });
   if (!user) {
     throw createHttpError(404, 'User not found');
   }
@@ -12,7 +14,7 @@ export const loginUser = async (payload) => {
   if (!isEqual) {
     throw createHttpError(401, 'Unauthorized');
   }
-  await Model.deleteOne({ userId: user._id });
+  await Session.deleteOne({ userId: user._id });
 
   const accessToken = randomBytes(30).toString('base64');
   const refreshToken = randomBytes(30).toString('base64');
